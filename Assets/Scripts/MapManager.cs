7e081511f6d306560c39d879e930756ec398a98a
Neal <n.asprion@gmx.de>
2020-05-15 02:57:03 +0200

    Fixed offset tiles in map rendering. Added check to make sure all tiles are of equal size. Tile sizes used in map rendering are now calculated on runtime.

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
@@ -16,12 +19,21 @@ public class MapManager : MonoBehaviour
     public GameObject grassTile;
     public GameObject stoneTile;
 
-    const int tileWidth = 10;
+    private List<GameObject> tiles;
+
+    private float tileWidth = 0;
+    private float tileDepth = 0;
+
+    //The factor applied to the grayscale to set the y value of each tile
+    const int heightFactor = 20;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        loadTiles();
+        tiles = new List<GameObject>() { forestTile, sandTile, mountainTile, waterTile, grassTile, stoneTile };
+        LoadTiles();
     }
 
     // Update is called once per frame
@@ -31,23 +43,42 @@ public class MapManager : MonoBehaviour
     }
 
 
-    private void loadTiles()
+    private void LoadTiles()
     {
         Debug.Log("Heightmap dimensions: "+heightMap.width + "x" + heightMap.height);
 
         //Remove the showcasing tiles
         GameObject[] prefabs = GameObject.FindGameObjectsWithTag("Tile");
-        foreach(GameObject prefab in prefabs)
+        foreach (GameObject prefab in prefabs)
         {
             Destroy(prefab);
         }
 
+        //Calculate the bounds of the tiles and ensure all are of equal size
+        foreach (GameObject o in tiles)
+        {
+
+            Vector3 collider = o.GetComponent<Collider>().bounds.size;
+            Vector3 renderer = o.GetComponent<Renderer>().bounds.size;
+
+            float maxWidth = Math.Max(collider.z, renderer.z);
+            float maxDepth = Math.Max(collider.x, renderer.x);
+
+            if (tileWidth == 0) tileWidth = maxWidth;
+            else if (tileWidth != maxWidth) throw new Exception("The tiles are not of equal width. " + o.name + " is " + maxWidth + " while previously iterated tiles where " + tileWidth + " wide.");
+
+            if (tileDepth == 0) tileDepth = maxDepth;
+            else if (tileDepth != maxDepth) throw new Exception("The tiles are not of equal depth. " + o.name + " is " + maxDepth + " while previously iterated tiles where " + tileDepth + " deep.");
+
+        }
+
         Color current;
 
-        float shiftZBy = heightMap.width * tileWidth / 2f;
+        //Shift by half a row width
+        float shiftZBy = (heightMap.width * tileWidth / 2f);
 
-        //Only every second row is added to the total x;
-        float shiftXBy = heightMap.height * tileWidth / 3f + tileWidth;
+        //Shift by half the map width times the added tileDepth per row;
+        float shiftXBy = heightMap.height * tileDepth * 0.75f / 2f;
 
         for (int x = 0; x < heightMap.height; x++)
         {
@@ -67,7 +98,7 @@ public class MapManager : MonoBehaviour
                 else throw new Exception("Error in parsing height map. Recieved max rgb value >1: "+max+". On tile: "+x+", "+z);
                 
 
-                Instantiate(toLoad, new Vector3(x * tileWidth * 0.75f - shiftXBy, current.grayscale * tileWidth * 2, z * tileWidth + x % 2 * tileWidth / 2f - shiftZBy), Quaternion.identity);
+                Instantiate(toLoad, new Vector3(x * tileDepth * 0.75f - shiftXBy, current.grayscale * heightFactor, z * tileWidth + x % 2 * tileWidth / 2f - shiftZBy), Quaternion.identity);
             }
         }
 
@@ -75,22 +106,22 @@ public class MapManager : MonoBehaviour
     }
 
 
-    public float getMaxX()
+    public float GetMaxX()
     {
         return 10f;
     }
 
-    public float getMaxZ()
+    public float GetMaxZ()
     {
         return 10f;
     }
 
-    public float getMinX()
+    public float GetMinX()
     {
         return -10f;
     }
 
-    public float getMinZ()
+    public float GetMinZ()
     {
         return -10f;
     }
